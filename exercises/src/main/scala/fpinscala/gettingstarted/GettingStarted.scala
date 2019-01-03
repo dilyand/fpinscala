5d6dc849ca210b864da677f6d7d06741df6616b7
Dilyan Damyanov <dilyand@gmail.com>
2019-01-03 14:42:12 +0000

    Add gettingstarted exercise 1 and 2

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -36,7 +36,15 @@ object MyModule {
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, first: Int, second: Int): Int = {
+      if (n == 0) 0
+      else if (n == 1) second
+      else go(n - 1, second, first + second)
+    }
+    go(n, 0, 1)
+  }
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
@@ -140,7 +148,15 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(start: Int, end: Int): Boolean = {
+      if (start == end) true
+      else if (gt(as(start), as(start + 1))) false
+      else go(start + 1, end)
+    }
+    go(0, as.length - 1)
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
