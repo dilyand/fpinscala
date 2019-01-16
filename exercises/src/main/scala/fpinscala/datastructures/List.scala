519fec76fe11a7b5ff2b2d7c66ccaac3c0355464
Dilyan Damyanov <dilyand@gmail.com>
2019-01-16 14:09:56 +0000

    2019-01-16 14:09

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -80,7 +80,9 @@ object List { // `List` companion object. Contains functions for creating and wo
     case Cons(h, t) => Cons(h, init(t))
   }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = {
+    foldRight(l, 0)((x, y) => 1 + y)
+  }
 
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
 
